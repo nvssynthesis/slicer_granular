b31ca2d451163abdc6e3755142ce474d1063e629
Nicholas Solem <nsolem@ucsd.edu>
2024-05-05 14:06:50 -0700

    init granular main parameters page

---
+++ Source/Gui/GranularMainParametersPage.cpp
@@ -0,0 +1,12 @@
+/*
+  ==============================================================================
+
+    GranularMainParametersPage.cpp
+    Created: 5 May 2024 1:51:43pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#include "GranularMainParametersPage.h"
+
---
+++ Source/Gui/GranularMainParametersPage.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    GranularMainParametersPage.h
+    Created: 5 May 2024 1:51:43pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+
+struct GranularMainParametersPage	:	public juce::Component
+{
+	
+};
