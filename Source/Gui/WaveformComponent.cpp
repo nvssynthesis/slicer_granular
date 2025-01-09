2f71e2637a763621ac5cd6c256860a77abb942ff
Nicholas Solem <nsolem@ucsd.edu>
2025-01-09 15:50:48 -0800

    separate WaveformComponent declarations and implementation

---
+++ Source/Gui/WaveformComponent.cpp
@@ -0,0 +1,150 @@
+/*
+  ==============================================================================
+
+    WaveformComponent.cpp
+    Created: 9 Jan 2025 3:41:31pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#include "WaveformComponent.h"
+
+
+WaveformComponent::WaveformComponent(int sourceSamplesPerThumbnailSample, juce::AudioFormatManager &formatManagerToUse)
+:	thumbnailCache(5), thumbnail(sourceSamplesPerThumbnailSample, formatManagerToUse, thumbnailCache)
+{
+	thumbnail.addChangeListener(this);	// thumbnail is a ChangeBroadcaster
+}
+	
+int WaveformComponent::getNumMarkers(){
+	return markerList.getNumMarkers();
+}
+void WaveformComponent::addMarker(double pos){
+	juce::String name(pos);
+	markerList.setMarker(name, juce::RelativeCoordinate(pos));
+}
+void WaveformComponent::removeMarkers(){
+	auto numMarkers = markerList.getNumMarkers();
+	for (auto i = numMarkers - 1; i >= 0 ; --i){
+		markerList.removeMarker(i);
+	}
+}
+	
+void WaveformComponent::drawMarker(juce::Graphics& g, double pos){
+	
+	g.setColour(juce::Colours::blue);
+	
+	int xPos = getWidth() * pos;
+	
+	g.drawLine(xPos, getY(), xPos, getBottom(), 2.f);
+}
+	
+void WaveformComponent::paint(juce::Graphics& g)
+{
+	g.setColour (juce::Colours::darkgrey);
+	auto const bounds = getLocalBounds();
+	g.drawRect(bounds);
+	
+	if (thumbnail.getNumChannels() == 0) {
+		paintContentsIfNoFileLoaded (g);
+	}
+	else {
+		paintContentsIfFileLoaded (g);
+	}
+	
+	for (auto i = 0; i < markerList.getNumMarkers(); ++i){
+		double pos = markerList.getMarkerPosition(*markerList.getMarker(i), this);
+		drawMarker(g, pos);
+	}
+}
+
+void WaveformComponent::changeListenerCallback (juce::ChangeBroadcaster* source)
+{
+	if (source == &thumbnail){
+		thumbnailChanged();
+	}
+}
+	
+juce::AudioThumbnail *const WaveformComponent::getThumbnail(){
+/*
+ not sure if it's better to:
+ -	use this to expose thumbnail in order to setSource in processor (such getters are a code smell), or
+ -	just make a public setSource method in WaveformComponent to call from AudioProcessor. disadvantage: have to #include the WaveformComponent in AudioProcessor, which feels wrong because its basically a GUI element.
+ */
+	return &thumbnail;
+}
+
+void WaveformComponent::paintContentsIfNoFileLoaded (juce::Graphics& g)
+{
+	g.setColour (juce::Colours::darkgrey);
+	g.drawFittedText ("No File Loaded", getLocalBounds(), juce::Justification::centred, 1);
+}
+void WaveformComponent::paintContentsIfFileLoaded (juce::Graphics& g)
+{
+	g.setColour (juce::Colours::black);
+	thumbnail.drawChannels (g,
+							getLocalBounds(),
+							0.0,                                    // start time
+							thumbnail.getTotalLength(),             // end time
+							1.0f);                                  // vertical zoom
+}
+
+/**
+ The trick to implement:
+ Levels of position quantization
+ In the editor, user can set 'requested' position via Position slider.
+ These potentially get quantized based on onsetsInSeconds (held in the Processor).
+ The positionQuantizedReadOnlySlider should follow that quantized value
+ */
+
+WaveformAndPositionComponent::WaveformAndPositionComponent(int sourceSamplesPerThumbnailSample, juce::AudioFormatManager &formatManagerToUse,
+								 juce::AudioProcessorValueTreeState &apvts)
+:	wc(sourceSamplesPerThumbnailSample, formatManagerToUse)
+,	positionSlider(apvts, params_e::position, juce::Slider::SliderStyle::LinearHorizontal, juce::Slider::NoTextBox)
+{
+	positionSlider._slider.addListener(this);
+	addAndMakeVisible(wc);
+	addAndMakeVisible(&positionSlider._slider);
+}
+	
+void WaveformAndPositionComponent::resized()
+{
+	auto const localBounds = getLocalBounds();
+	
+	auto const totalHeight = localBounds.getHeight();
+	auto const reservedHeight = totalHeight * 0.9;
+
+	juce::Rectangle<int> const wcRect = [&]{	// limit scope via instantly-called lambda
+		auto const heightDiff = totalHeight - reservedHeight;
+		auto const waveformY = localBounds.getY() + (heightDiff * 0.5);
+		auto const waveformHeight = reservedHeight * 0.8f;
+		
+		auto const waveformWidth = localBounds.getWidth() * 1.0;
+		auto const waveformWidthDiff = localBounds.getWidth() - waveformWidth;
+		auto const waveformX = localBounds.getX() + (waveformWidthDiff * 0.5);
+
+		auto const wcRect = juce::Rectangle<int>(waveformX, waveformY, waveformWidth, waveformHeight);
+		wc.setBounds(wcRect);
+		return wcRect;	// return rectangle as that's all we need from this scope
+	}();
+	
+	{	// limit scope for drawing slider
+		auto const sliderHeight = reservedHeight - wcRect.getHeight();
+		int const widthIncrease = 14;
+		auto const sliderWidth = wcRect.getWidth() + widthIncrease;
+		auto const sliderX = wcRect.getX() - (widthIncrease*0.5);
+		auto const sliderY = wcRect.getBottom();
+		auto const sliderRect = juce::Rectangle<int>(sliderX, sliderY, sliderWidth, sliderHeight);
+		positionSlider._slider.setBounds(sliderRect);
+	}
+}
+void WaveformAndPositionComponent::paint (juce::Graphics& g) {
+	
+}
+
+void WaveformAndPositionComponent::sliderValueChanged (juce::Slider *slider) {
+	if (slider == &positionSlider._slider){
+		// this used to set the position of the read-only slider
+	}
+}
--- Source/Gui/WaveformComponent.h
+++ Source/Gui/WaveformComponent.h
@@ -24,72 +24,21 @@
 
 class WaveformComponent		:	public juce::Component
 ,								private juce::ChangeListener
-//,								private juce::MarkerList::Listener
 {
 public:
-	WaveformComponent(int sourceSamplesPerThumbnailSample, juce::AudioFormatManager &formatManagerToUse)
-	:	thumbnailCache(5), thumbnail(sourceSamplesPerThumbnailSample, formatManagerToUse, thumbnailCache)
-	{
-		thumbnail.addChangeListener(this);	// thumbnail is a ChangeBroadcaster
-	}
+	WaveformComponent(int sourceSamplesPerThumbnailSample, juce::AudioFormatManager &formatManagerToUse);
 	
-	int getNumMarkers(){
-		return markerList.getNumMarkers();
-	}
-	void addMarker(double pos){
-		juce::String name(pos);
-		markerList.setMarker(name, juce::RelativeCoordinate(pos));
-	}
-	void removeMarkers(){
-		auto numMarkers = markerList.getNumMarkers();
-		for (auto i = numMarkers - 1; i >= 0 ; --i){
-			markerList.removeMarker(i);
-		}
-	}
+	int getNumMarkers();
+	void addMarker(double pos);
+	void removeMarkers();
 	
-	void drawMarker(juce::Graphics& g, double pos){
-		
-		g.setColour(juce::Colours::blue);
-		
-		int xPos = getWidth() * pos;
-		
-		g.drawLine(xPos, getY(), xPos, getBottom(), 2.f);
-	}
+	void drawMarker(juce::Graphics& g, double pos);
 	
-	void paint(juce::Graphics& g) override
-	{
-		g.setColour (juce::Colours::white);
-		auto const bounds = getLocalBounds();
-		g.drawRect(bounds);
-		
-		if (thumbnail.getNumChannels() == 0) {
-			paintContentsIfNoFileLoaded (g);
-		}
-		else {
-			paintContentsIfFileLoaded (g);
-		}
-		
-		for (auto i = 0; i < markerList.getNumMarkers(); ++i){
-			double pos = markerList.getMarkerPosition(*markerList.getMarker(i), this);
-			drawMarker(g, pos);
-		}
-	}
+	void paint(juce::Graphics& g) override;
 	void resized() override {}
-	void changeListenerCallback (juce::ChangeBroadcaster* source) override
-	{
-		if (source == &thumbnail){
-			thumbnailChanged();
-		}
-	}
+	void changeListenerCallback (juce::ChangeBroadcaster* source) override;
 	
-	juce::AudioThumbnail *const getThumbnail(){
-	/*
-	 not sure if it's better to:
-	 -	use this to expose thumbnail in order to setSource in processor (such getters are a code smell), or
-	 -	just make a public setSource method in WaveformComponent to call from AudioProcessor. disadvantage: have to #include the WaveformComponent in AudioProcessor, which feels wrong because its basically a GUI element.
-	 */
-		return &thumbnail;
-	}
+	juce::AudioThumbnail *const getThumbnail();
 	
 private:
 	juce::AudioThumbnailCache thumbnailCache;
@@ -101,20 +50,8 @@ private:
 	{
 		repaint();
 	}
-	void paintContentsIfNoFileLoaded (juce::Graphics& g)
-	{
-		g.setColour (juce::Colours::darkgrey);
-		g.drawFittedText ("No File Loaded", getLocalBounds(), juce::Justification::centred, 1);
-	}
-	void paintContentsIfFileLoaded (juce::Graphics& g)
-	{
-		g.setColour (juce::Colours::black);
-		thumbnail.drawChannels (g,
-								getLocalBounds(),
-								0.0,                                    // start time
-								thumbnail.getTotalLength(),             // end time
-								1.0f);                                  // vertical zoom
-	}
+	void paintContentsIfNoFileLoaded (juce::Graphics& g);
+	void paintContentsIfFileLoaded (juce::Graphics& g);
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WaveformComponent);
 };
 
@@ -129,59 +66,16 @@ class WaveformAndPositionComponent	:	public juce::Component
 ,										public juce::Slider::Listener
 {
 public:
-	WaveformAndPositionComponent(int sourceSamplesPerThumbnailSample, juce::AudioFormatManager &formatManagerToUse,
-								 juce::AudioProcessorValueTreeState &apvts)
-	:	wc(sourceSamplesPerThumbnailSample, formatManagerToUse)
-	,	positionSlider(apvts, params_e::position, juce::Slider::SliderStyle::LinearHorizontal, juce::Slider::NoTextBox)
-	{
-		positionSlider._slider.addListener(this);
-		addAndMakeVisible(wc);
-		addAndMakeVisible(&positionSlider._slider);
-	}
+	WaveformAndPositionComponent(int sourceSamplesPerThumbnailSample,
+								 juce::AudioFormatManager &formatManagerToUse,
+								 juce::AudioProcessorValueTreeState &apvts);
 	
-	void resized() override
-	{
-		auto const localBounds = getLocalBounds();
-		
-		auto const totalHeight = localBounds.getHeight();
-		auto const reservedHeight = totalHeight * 0.9;
-
-		juce::Rectangle<int> const wcRect = [&]{	// limit scope via instantly-called lambda
-			auto const heightDiff = totalHeight - reservedHeight;
-			auto const waveformY = localBounds.getY() + (heightDiff * 0.5);
-			auto const waveformHeight = reservedHeight * 0.8f;
-			
-			auto const waveformWidth = localBounds.getWidth() * 1.0;
-			auto const waveformWidthDiff = localBounds.getWidth() - waveformWidth;
-			auto const waveformX = localBounds.getX() + (waveformWidthDiff * 0.5);
-
-			auto const wcRect = juce::Rectangle<int>(waveformX, waveformY, waveformWidth, waveformHeight);
-			wc.setBounds(wcRect);
-			return wcRect;	// return rectangle as that's all we need from this scope
-		}();
-		
-		{	// limit scope for drawing slider
-			auto const sliderHeight = reservedHeight - wcRect.getHeight();
-			int const widthIncrease = 14;
-			auto const sliderWidth = wcRect.getWidth() + widthIncrease;
-			auto const sliderX = wcRect.getX() - (widthIncrease*0.5);
-			auto const sliderY = wcRect.getBottom();
-			auto const sliderRect = juce::Rectangle<int>(sliderX, sliderY, sliderWidth, sliderHeight);
-			positionSlider._slider.setBounds(sliderRect);
-		}
-	}
-	void paint (juce::Graphics& g) override {
-		
-	}
+	void resized() override;
+	void paint (juce::Graphics& g) override;
 	
-	void sliderValueChanged (juce::Slider *slider) override {
-		if (slider == &positionSlider._slider){
-			// this used to set the position of the read-only slider
-		}
-	}
+	void sliderValueChanged (juce::Slider *slider) override;
 
-	// make this accessible from outside so there's no need for a bunch of forwarding methods
-	WaveformComponent wc;
+	WaveformComponent wc; // externally accessible so there's no need for a bunch of forwarding methods
 private:
 	AttachedSlider positionSlider;
 	std::atomic<double> position;
