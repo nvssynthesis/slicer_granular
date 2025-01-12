55782bca59a08f0467777e48f86449bdc8d0168c
Nicholas Solem <nsolem@ucsd.edu>
2025-01-12 15:49:32 -0800

    move GrainDescription to .h file

---
+++ Source/Synthesis/GrainDescription.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    GrainDescription.h
+    Created: 11 Jan 2025 2:26:20pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#pragma once
+namespace nvs::gran {
+struct GrainDescription {
+	// a struct to communicate upstream (from actual realtime granular synthesis) about the coarse description of a given grain's current state
+	int grain_id;
+	double position;
+	double sample_playback_rate;
+	float window;
+	float pan;
+};
+}	// namespace nvs::gran
