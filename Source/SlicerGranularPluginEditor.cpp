50d25be357c14a4c7982e72819a5865725ec45d6
Nicholas Solem <nsolem@ucsd.edu>
2025-02-21 22:26:11 -0800

    bring out placeFileCompAndGrainBusyDisplay into free function

--- Source/SlicerGranularPluginEditor.cpp
+++ Source/SlicerGranularPluginEditor.cpp
@@ -159,6 +159,31 @@ void Slicer_granularAudioProcessorEditor::paint (juce::Graphics& g)
     g.setFont (15.0f);
 }
 
+int placeFileCompAndGrainBusyDisplay(juce::Rectangle<int> localBounds, int pad, GrainBusyDisplay &grainBusyDisplay, FileSelectorComponent &fileComp, int y) {
+	int const fileCompAndGrainDisplayHeight = 26;
+	{
+		int const pad = 2;
+		int const grainDisplayHeight = fileCompAndGrainDisplayHeight - pad;
+		
+		grainBusyDisplay.setSizePerGrain((float)grainDisplayHeight / (float)N_VOICES);
+		float const sizePerGrain = grainBusyDisplay.getSizePerGrain();
+		
+		int const grainBusyDisplayWidth = (float)N_GRAINS * sizePerGrain - pad;
+		
+		int const grainBusyX = localBounds.getX() + (localBounds.getWidth() - grainBusyDisplayWidth) + pad/2;
+		int const grainBusyY = y + pad/2;
+		grainBusyDisplay.setBounds(grainBusyX, grainBusyY, grainBusyDisplayWidth, grainDisplayHeight);
+	}
+	{
+		int const fileCompWidth = localBounds.getWidth() - grainBusyDisplay.getWidth();
+		int const x(localBounds.getX());
+		fileComp.setBounds(x, y, fileCompWidth, fileCompAndGrainDisplayHeight);
+		y += fileCompAndGrainDisplayHeight;
+		y += pad;
+	}
+	return y;	// needs to know the new y to place components at
+}
+
 void Slicer_granularAudioProcessorEditor::resized()
 {
 	constrainer.checkComponentBounds(this);
@@ -167,29 +192,7 @@ void Slicer_granularAudioProcessorEditor::resized()
 	localBounds.reduce(smallPad, smallPad);
 	
 	int y(localBounds.getY());
-	{ // just some scopes for temporaries
-		int const fileCompAndGrainDisplayHeight = 26;
-		{
-			int const pad = 2;
-			int const grainDisplayHeight = fileCompAndGrainDisplayHeight - pad;
-			
-			grainBusyDisplay.setSizePerGrain((float)grainDisplayHeight / (float)N_VOICES);
-			float const sizePerGrain = grainBusyDisplay.getSizePerGrain();
-			
-			int const grainBusyDisplayWidth = (float)N_GRAINS * sizePerGrain - pad;
-			
-			int const grainBusyX = localBounds.getX() + (localBounds.getWidth() - grainBusyDisplayWidth) + pad/2;
-			int const grainBusyY = y + pad/2;
-			grainBusyDisplay.setBounds(grainBusyX, grainBusyY, grainBusyDisplayWidth, grainDisplayHeight);
-		}
-		{
-			int const fileCompWidth = localBounds.getWidth() - grainBusyDisplay.getWidth();
-			int const x(localBounds.getX());
-			fileComp.setBounds(x, y, fileCompWidth, fileCompAndGrainDisplayHeight);
-			y += fileCompAndGrainDisplayHeight;
-			y += smallPad;
-		}
-	}
+	y = placeFileCompAndGrainBusyDisplay(localBounds, smallPad, grainBusyDisplay, fileComp, y);
 	{
 		auto const mainParamsRemainingHeightRatio = 0.8 * localBounds.getHeight();
 
