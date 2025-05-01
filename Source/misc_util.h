efa5401e03b8e132b5f9c71e1ad495d90ea8592f
Nicholas Solem <nsolem@ucsd.edu>
2025-05-01 13:44:33 -0700

    add generic iterator for iterating thru enum classes

--- Source/misc_util.h
+++ Source/misc_util.h
@@ -40,4 +40,25 @@ struct MeasuredData : public juce::ChangeBroadcaster
 	std::atomic<bool> dataReady {false};
 	std::atomic<int> activeBufferIdx {0};
 };
+
+template < typename C, C beginVal, C endVal>
+class Iterator {
+  typedef typename std::underlying_type<C>::type val_t;
+  int val;
+public:
+  Iterator(const C & f) : val(static_cast<val_t>(f)) {}
+  Iterator() : val(static_cast<val_t>(beginVal)) {}
+  Iterator operator++() {
+	++val;
+	return *this;
+  }
+  C operator*() { return static_cast<C>(val); }
+  Iterator begin() { return *this; } //default ctor is good
+  Iterator end() {
+	  static const Iterator endIter=++Iterator(endVal); // cache it
+	  return endIter;
+  }
+  bool operator!=(const Iterator& i) { return val != i.val; }
+};
+
 }	// nvs::util
