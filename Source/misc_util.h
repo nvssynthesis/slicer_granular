a1bf28fcb46758115188380dd60578545f2cb7a5
Nicholas Solem <nsolem@ucsd.edu>
2025-07-02 14:23:26 -0700

    move out timbre space stuff from util to timbrespace namespace (not part of slicer)

--- Source/misc_util.h
+++ Source/misc_util.h
@@ -14,22 +14,6 @@
 
 namespace nvs::util {
 
-using timbre2DPoint = juce::Point<float>;
-using timbre3DPoint = std::array<float, 3>;
-
-struct Timbre5DPoint {
-
-	timbre2DPoint _p2D;			// used to locate the point in x,y plane
-	timbre3DPoint _p3D;	// used to describe the colour (hsv)
-
-	bool operator==(Timbre5DPoint const &other) const;
-	timbre2DPoint get2D() const { return _p2D; }
-	timbre3DPoint get3D() const { return _p3D; }
-
-	// to easily trade hsv for rbg
-	std::array<juce::uint8, 3> toUnsigned() const;
-};
-
 struct WeightedIdx
 {
 	int idx	{0};
@@ -46,35 +30,6 @@ struct DistanceIdx	// effectively same class as weightedIdx but reminds you that
 	DistanceIdx(int i, double d) : idx(i), distance(d) {}
 	DistanceIdx() = default;
 };
-
-class TimbreSpaceHolder {
-public:
-	void add5DPoint(timbre2DPoint p2D, std::array<float, 3> p3D);
-	void clear();
-	
-	juce::Array<nvs::util::Timbre5DPoint> &getTimbreSpace() {
-		return timbres5D;
-	}
-	std::vector<WeightedIdx> getCurrentPointIndices() const {
-		return currentPointIndices;
-	}
-	void setProbabilisticPointFromTarget(const Timbre5DPoint& target, int K_neighbors, double sharpness, float higher3Dweight=0.f);
-
-private:
-	juce::Array<nvs::util::Timbre5DPoint> timbres5D;
-	
-	// safe private setter
-	void setCurrentPointIndices(std::vector<WeightedIdx> &&newWeightedIndices);
-	
-	std::vector<WeightedIdx> currentPointIndices {{},{},{},{}};
-};
-
-constexpr bool inRange0_1(float x){
-	return ( (x >= 0.f) && (x <= 1.f) );
-}
-constexpr bool inRangeM1_1(float x){
-	return ( (x >= -1.f) && (x <= 1.f) );
-}
 
 struct LoggingGuts {
 	LoggingGuts();	// constructor wants to be defined in the corresponding plugin processor
