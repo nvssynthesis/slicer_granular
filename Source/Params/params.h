88703d8f23c97a0bb05107e9e495ba3a3b0413c3
Nicholas Solem <nsolem@ucsd.edu>
2025-10-23 22:14:18 +0200

    hyperchaos uses skewed d_t

--- Source/Params/params.h
+++ Source/Params/params.h
@@ -380,7 +380,7 @@ inline const std::vector<ParameterDef> ALL_PARAMETERS = {
 
     ParameterDef::linear("nav_hyperchaos_a", "a", "TSN", 10e-5, 10e-1, 10e-4, "", 0.f, "nav_hyperchaos"),
     ParameterDef::linear("nav_hyperchaos_b", "b", "TSN", 10e-5, 10e-1, 10e-4, "", 0.f, "nav_hyperchaos"),
-    ParameterDef::linear("nav_hyperchaos_d_t", "d_t", "TSN", 0.f, 0.5f, 0.005f, "", 0.f, "nav_hyperchaos"),
+    ParameterDef::skewed("nav_hyperchaos_d_t", "d_t", "TSN", 0.f, 0.5f, 0.005f, "", 0.33f, false, "nav_hyperchaos"),
 
     ParameterDef::linear("nav_rotation_x", "Roll", "TSN", 0.f, 1.f, 0.f, "", 0.f, "nav_common"),
     ParameterDef::linear("nav_rotation_y", "Pitch", "TSN", 0.f, 1.f, 0.f, "", 0.f, "nav_common"),
