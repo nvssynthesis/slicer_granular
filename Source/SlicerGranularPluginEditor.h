6d071986efa160ddc2eb694767cefdd0158f5c55
Nicholas Solem <nsolem@ucsd.edu>
2026-02-18 22:17:42 +0800

    dont use relative include paths for juce_utils; let cmake resolve

--- Source/SlicerGranularPluginEditor.h
+++ Source/SlicerGranularPluginEditor.h
@@ -14,7 +14,7 @@
 #include "Gui/WaveformComponent.h"
 #include "Gui/TabbedPages.h"
 #include "Gui/GrainBusyDisplay.h"
-#include "../juce_utils/juce_utils.h"
+#include "juce_utils.h"
 
 //==============================================================================
 
