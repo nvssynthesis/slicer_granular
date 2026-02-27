b58496a400da1d6f589ff870945fdad1248ffb46
Nicholas Solem <nsolem@ucsd.edu>
2026-02-27 13:16:16 -0500

    adjust skew of duration param

--- Source/Params/params.h
+++ Source/Params/params.h
@@ -329,7 +329,7 @@ inline const std::vector<ParameterDef> ALL_PARAMETERS = {
 	ParameterDef::linear("transpose", 	"Transpose", 	"Main", -60.f,	    60.f,		0.f, 	" semi"),
 	ParameterDef::percent("position", "Position", 		"Main",   0.f,	     1.f,		0.f),
 	ParameterDef::skewed("speed", "Speed", 			 	"Main",  0.1f, 	  1000.f, 		50.f,	"hz"),
-	ParameterDef::percent("duration", "Duration", 	 	"Main", 1e-6f, 	  	 1.f, 		0.1f,	"", 0.3f, false),	// percent with skew
+	ParameterDef::percent("duration", "Duration", 	 	"Main", 1e-5f, 	  	 1.f, 		0.1f,	"", 0.4f, false),	// percent with skew
 	ParameterDef::percent("skew", 	"Skew", 			"Main",	skeps, 	1.f-skeps, 		0.5f),						// percent with clipped range
 	ParameterDef::skewed("plateau", "Plateau", 		 	"Main",	0.01f, 		10.f, 		1.f, 	""),
 	ParameterDef::percent("pan", 	"Pan", 			 	"Main", 0.f,		 1.f,		0.5f),
